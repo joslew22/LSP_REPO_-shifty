83123efc07d0eea93d8897fc828cd79081e2bc2e
pop <pop@localhost>
2025-01-20 22:31:45 -0500

    first commit

---
+++ helloworld/src/org/howard/edu/lsp/assignment1/hellowrld.java
@@ -0,0 +1,13 @@
+// Name: Joseph Lewis
+
+package org.howard.edu.lsp.assignment1;
+
+public class hellowrld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
